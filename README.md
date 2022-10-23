00a00f15248f1ac7e0b443a5ea56bd1b99f6b253
heshuo <2393097587@qq.com>
2022-10-23 22:17:40 +0800

    1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+### 这是一个 todoList 项目
+### 技术栈 react + ts + antd + hooks
+
+### npm i 下载依赖
+### npm start 运行
---
+++ src/App.tsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+import { Input, Button, message, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import './css/index.css'
+
+
+function App() {
+
+  //创建一个 TodoType 接口
+  type TodoType = {
+    key: number,
+    id: number,
+    content: string,
+    time: number,
+    done: boolean,
+    todoList?: TodoType
+  }
+
+  //初始化输入框的内容 和 小li
+  const [inpValue, setInpValue] = useState('')
+  const [todoList, setTodoList] = useState<Array<TodoType>>([
+    {
+      key: 1666455021112,
+      id: 1666455021112,
+      content: '敲代码',
+      time: 1666455021112,
+      done: true,
+    }
+  ])
+
+  //实现本地存储 序列化数组
+  const saveTodo = (arr: Array<TodoType>) => {
+    if (arr.length !== 0) {
+      localStorage.todoList = JSON.stringify(arr)
+    }
+  }
+
+  //声明周期钩子
+  useEffect(() => {
+
+  })
+
+  const newTime = (time: Date | number) => {
+    const d = new Date(time)
+    const yarn = d.getFullYear();
+    const month = (d.getMonth() + 1) < 10 ? ('0' + (d.getMonth() + 1)) : (d.getMonth() + 1);
+    const data = d.getDate() < 10 ? ('0' + d.getDate()) : (d.getDate());
+    const hours = d.getHours() < 10 ? ('0' + d.getHours()) : (d.getHours());
+    const minute = d.getMinutes() < 10 ? ('0' + d.getMinutes()) : (d.getMinutes());
+    const second = d.getSeconds() < 10 ? ('0' + d.getSeconds()) : (d.getSeconds());
+    return yarn + '-' + month + '-' + data + ' ' + hours + ':' + minute + ':' + second
+  }
+
+  const columns: ColumnsType<TodoType> = [
+    {
+      title: '内容',
+      dataIndex: 'content',
+      key: 'time',
+      render: ((_, todo) => {
+        return <div className={todo.done ? 'todo-content' : 'content'}>{todo.content}</div>
+      })
+    },
+    {
+      title: '时间',
+      dataIndex: 'time',
+      key: 'time',
+      render: ((_, todo) => {
+        return newTime(_)
+      })
+    },
+    {
+      title: '操作',
+      dataIndex: 'address',
+      key: 'address',
+      render: ((_, todo) => {
+        return (
+          <>
+            <Button onClick={() => todoDone(todo.id)} type="primary">完成</Button>
+            <Button onClick={() => todoDelete(todo.id)} type="primary" danger>删除</Button>
+          </>
+        )
+      })
+    },
+  ];
+
+  //增加一个todoList的回调
+  const addTodo = () => {
+    if (inpValue === '') {
+      return message.error('请输入内容');
+    } else {
+      const items = {
+        key: new Date().getTime(),
+        id: new Date().getTime(),
+        content: inpValue,
+        time: new Date().getTime(),
+        done: false
+      }
+      message.success('已完成')
+      setTodoList([...todoList, items]);
+    saveTodo([...todoList, items])
+    };
+  }
+
+  //完成一个done的回调
+  const todoDone = (id: number | undefined) => {
+    //浅拷贝一下
+    const todos = [...todoList]
+    todos.forEach(todo => {
+      if (todo.id === id) {
+        if (todo.done === false) {
+          todo.done = true
+        } else {
+          todo.done = false
+        }
+      };
+      setTodoList(todos)
+    });
+    message.warning('已完成')
+  }
+
+  //删除一个todoList的回调
+  const todoDelete = (id: number | undefined) => {
+    //浅拷贝一下
+    const todos = [...todoList]
+    const newList = todos.filter(todo => todo.id !== id)
+    message.error('已删除')
+    setTodoList(newList)
+  }
+
+  return (
+    <>
+      <Input className='antd-input' placeholder="请输入内容" onChange={(e) => setInpValue(e.target.value)} />
+      <Button type="primary" onClick={addTodo}>增加</Button>
+      {
+        <Table columns={columns} dataSource={todoList} />
+      }
+    </>
+  );
+}
+
+export default App;
